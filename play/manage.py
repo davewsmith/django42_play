024f7193db66e17d95c670d695edd730e7bb217f
Dave Smith <davewsmith@gmail.com>
2023-11-19 11:05:01 -0800

    Add django-dotenv

--- play/manage.py
+++ play/manage.py
@@ -3,9 +3,12 @@
 import os
 import sys
 
+import dotenv
+
 
 def main():
     """Run administrative tasks."""
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'play.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -19,4 +22,5 @@ def main():
 
 
 if __name__ == '__main__':
+    dotenv.read_dotenv()
     main()
